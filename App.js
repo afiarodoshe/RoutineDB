5660b9a109a567d8a93ca351a8f9c108bf6c8c6c
afiarodoshe <afiamuntakim@iut-dhaka.edu>
2020-10-08 21:06:17 +0600

    SimpleApp done

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
+import HomeScreen from "./src/Screens/HomeScreen";
+import profile from "./src/Screens/profile";
+import semesters from "./src/Screens/Semesters";
+import Faculties from "./src/Screens/FacultyList"
+import coursesem3 from "./src/Screens/CourseListsem3"
+import coursesem4 from "./src/Screens/CourseListsem4"
+import coursesem5 from "./src/Screens/CourseListsem5"
+import coursesem6 from "./src/Screens/CourseListsem6"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+const Stack = createStackNavigator();
+
+function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName = "Home">
+                <Stack.Screen name = "Home" component={HomeScreen}/>
+                <Stack.Screen name = "Semesters" component={semesters}/>
+                <Stack.Screen name = "Faculties" component={Faculties}/>
+                <Stack.Screen name = "Profile" component={profile}/>
+                <Stack.Screen name = "Semester 3" component={coursesem3}/>
+                <Stack.Screen name = "Semester 4" component={coursesem4}/>
+                <Stack.Screen name = "Semester 5" component={coursesem5}/>
+                <Stack.Screen name = "Semester 6" component={coursesem6}/>
+
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
